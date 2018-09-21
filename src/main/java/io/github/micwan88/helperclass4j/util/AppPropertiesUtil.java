666a376643ce86ee1960ba058a979e4b5b01412c
Michael Wan <hkmicmic@gmail.com>
2018-09-22 00:24:09 +0800

    Updated AppPrepertiesUtil to support multiple app properties file

--- src/main/java/io/github/micwan88/helperclass4j/util/AppPropertiesUtil.java
+++ src/main/java/io/github/micwan88/helperclass4j/util/AppPropertiesUtil.java
@@ -8,6 +8,7 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
@@ -17,9 +18,9 @@ public class AppPropertiesUtil {
 	public static final String APP_PROPERTY_FILE = "app.properties";
 	
 	private static final Logger myLogger = LogManager.getLogger(AppPropertiesUtil.class);
-	private static Properties appProperties = null;
+	private static HashMap<String, Properties> appPropertiesMap = new HashMap<>();
 	
-	public Properties loadOtherProperties(Path propertyFilePath) {
+	public static Properties loadProperties(Path propertyFilePath) {
 		BufferedReader br = null;
 		
 		myLogger.debug("try loading property file {}", propertyFilePath.toAbsolutePath());
@@ -47,60 +48,81 @@ public class AppPropertiesUtil {
 		return null;
 	}
 	
-	private Properties loadAppProperties() {
-		if (appProperties != null)
-			return appProperties;
-		
+	public static Properties loadProperties(String resourcesName) {
 		BufferedReader br = null;
-		if (appProperties == null) {
-			myLogger.debug("appProperties is null, try loading {}", APP_PROPERTY_FILE);
+		
+		myLogger.debug("try loading property file {} in classpath", resourcesName);
+		
+		InputStream inStream = AppPropertiesUtil.class.getClass().getResourceAsStream("/" + resourcesName);
+		if (inStream == null) {
+			myLogger.error("{} not found in classpath !", resourcesName);
+			return null;
+		}
+		
+		try {
+			br = new BufferedReader(new InputStreamReader(inStream, "UTF-8"));
+			Properties tmpProperties = new Properties();
+			tmpProperties.load(br);
 			
-			InputStream inStream = this.getClass().getResourceAsStream("/" + APP_PROPERTY_FILE);
-			if (inStream == null) {
-				myLogger.error("{} not found !", APP_PROPERTY_FILE);
-				return null;
-			}
+			myLogger.debug("Load property file done");
 			
+			return tmpProperties;
+		} catch (UnsupportedEncodingException e) {
+			myLogger.error("Cannot read property file", e);
+		} catch (IOException e) {
+			myLogger.error("Cannot read property file", e);
+		} finally {
 			try {
-				br = new BufferedReader(new InputStreamReader(inStream, "UTF-8"));
-				Properties tmpProperties = new Properties();
-				tmpProperties.load(br);
-				
-				myLogger.debug("Load appProperties done");
-				
-				appProperties = tmpProperties;
-				return appProperties;
-			} catch (UnsupportedEncodingException e) {
-				myLogger.error("Cannot read property file", e);
+				if (br != null)
+					br.close();
 			} catch (IOException e) {
-				myLogger.error("Cannot read property file", e);
-			} finally {
-				try {
-					if (br != null)
-						br.close();
-				} catch (IOException e) {
-					//Do Nothing
-				}
+				//Do Nothing
 			}
 		}
 		return null;
 	}
 	
 	public Properties getAppProperty() {
-		return loadAppProperties();
+		return getAppProperty(APP_PROPERTY_FILE);
+	}
+	
+	public Properties getAppProperty(String resourcesName) {
+		return getAppProperty(resourcesName, false);
+	}
+	
+	public Properties getAppProperty(String resourcesName, boolean reload) {
+		if (!reload && appPropertiesMap.containsKey(resourcesName))
+			return appPropertiesMap.get(resourcesName);
+		
+		myLogger.debug("Properties {} is not yet load or force reload, try loading it ...", resourcesName);
+		
+		Properties tmpProperties = loadProperties(resourcesName);
+		if (tmpProperties != null)
+			appPropertiesMap.put(resourcesName, tmpProperties);
+		return tmpProperties;
 	}
 	
 	public String getAppPropertyValue(String key, String defaultValue) {
-		if (appProperties == null && loadAppProperties() == null)
+		return getAppPropertyValue(APP_PROPERTY_FILE, key, defaultValue);
+	}
+	
+	public String getAppPropertyValue(String resourcesName, String key, String defaultValue) {
+		Properties tmpProperties = getAppProperty(resourcesName);
+		if (tmpProperties == null)
 			return null;
 		
-		return appProperties.getProperty(key, defaultValue);
+		return tmpProperties.getProperty(key, defaultValue);
 	}
 	
 	public long getAppPropertyValueLong(String key, long defaultValue) {
-		if (appProperties == null && loadAppProperties() == null)
+		return getAppPropertyValueLong(APP_PROPERTY_FILE, key, defaultValue);
+	}
+	
+	public long getAppPropertyValueLong(String resourcesName, String key, long defaultValue) {
+		Properties tmpProperties = getAppProperty(resourcesName);
+		if (tmpProperties == null)
 			return 0L;
 		
-		return (appProperties.containsKey(key)?Long.parseLong(appProperties.getProperty(key)):defaultValue);
+		return (tmpProperties.containsKey(key)?Long.parseLong(tmpProperties.getProperty(key)):defaultValue);
 	}
 }
