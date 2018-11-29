db5091897de433005b32c8bfdbcfaba9daa203ce
MicMic <hkmicmic@gmail.com>
2018-11-29 17:53:33 +0800

    Add TestClass for ByteClassLoader

--- src/test/java/io/github/micwan88/helperclass4j/TestByteClassLoader.java
+++ src/test/java/io/github/micwan88/helperclass4j/TestByteClassLoader.java
@@ -1,5 +1,28 @@
 package io.github.micwan88.helperclass4j;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestByteClassLoader {
+	private byte[] jarInBytes = null;
 	
+	@Before
+	public void loadJarToBytes() throws DecoderException {
+		//Load a jar with a class and resource
+		jarInBytes = Hex.decodeHex("");
+	}
+	
+	@Test
+	public void startTest() throws ClassNotFoundException {
+		ByteClassLoader byteClassLoader = new ByteClassLoader(this.getClass().getClassLoader());
+		byteClassLoader.initClassDataInBytes(jarInBytes, true);
+		
+		Class<?> helloWorldClass = byteClassLoader.loadClass("test.HelloWorld");
+		byteClassLoader.getResourceAsStream("msg.properties");
+	}
+	
+	public static void main(String[] args) {
+	}
 }
